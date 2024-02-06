e48218994613bef20411dff419f4234f0bc13d8f
Arjan de Ruijter <rra@mboutrecht.nl>
2024-02-06 10:50:59 +0100

    Core class is af

--- app/libraries/Core.php
+++ app/libraries/Core.php
@@ -5,8 +5,14 @@
  */
 class Core 
 {
-    // In de $currentController stoppen we de naam van de controller
+    // In de variabele $currentController stoppen we de naam van de controller
     protected $currentController = 'Homepages';
+
+    // In de variabele $currentMethod stoppen we de naam van de method in de controllerclass
+    protected $currentMethod = 'index';
+
+    // In de variabele $params slaan we alle parameters op van de method
+    protected $params = [];
 
     public function __construct()
     {
@@ -47,6 +53,43 @@ class Core
              */
             unset($url[0]);
         }
-        
+
+        /**
+         * Sluit de controller class in en maak deze beschikbaar om een object van te maken
+         */
+        require_once '../app/controllers/' . $this->currentController . '.php';
+
+        /**
+         * Maak een nieuw object van de controllerclass
+         */
+        $this->currentController = new $this->currentController();
+
+        /**
+         * Check of de method (tweede woord in de URL) bestaat in de controllerclass
+         */
+        if (method_exists($this->currentController, $url[1])) {
+
+            /**
+             * Bewaar de naam van de method in $this->currentMethod
+             */
+            $this->currentMethod = $url[1];
+
+            /**
+             * Verwijder de naam van de method uit het array $url
+             */
+            unset($url[1]);
+        }
+
+        /**
+         * Als $url bestaat en niet null worden de waarden daarvan
+         * in het array $this->params gezet. Anders wordt er een leeg
+         * array in $this->params gezet. Dit is een ternary operator
+         */
+        $this->params = $url ? array_values($url): [];
+
+        /**
+         * Roep de method met alle parameters aan van de class 
+         */
+        call_user_func_array([$this->currentController, $this->currentMethod], $this->params);
     }
 }
